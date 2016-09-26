6b54985d1f91b47d2d3ef229730978e8bec1ab48
Owen Williams <owilliams@mixxx.org>
2016-09-25 23:35:47 -0400

    another day another tab fix

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -22,72 +22,70 @@ class VotersController < ApplicationController
       @voter.email = @voter.email.downcase
 
       if(Voter.where(email: @voter.email).take)
-          render "signup_exists"
-          return
+        render "signup_exists"
+        return
       end
       
       if @voter.save
-          session[:voter_id] = @voter.id # log in
+        session[:voter_id] = @voter.id # log in
 
-          # save survey
-          voter_survey = VoterSurvey.new(voter_survey_params)
-          voter_survey.voter_id = @voter.id
-          voter_survey.save
+        # save survey
+        voter_survey = VoterSurvey.new(voter_survey_params)
+        voter_survey.voter_id = @voter.id
+        voter_survey.save
 
-          render "signup_success" 
-          else
-          render "signup_failure"
+        render "signup_success" 
+      else
+        render "signup_failure"
       end
       # render plain: params[:artist].inspect
       # render "signup_success"
     end
     
     def index
-        if(!current_voter)
-            return
+      if(!current_voter)
+        return
+      end
+
+      @grant_submissions = GrantSubmission.where(grant_id: active_grants)
+
+      # this is good for lace/temple
+      # @grant_submissions = @grant_submissions.sort { |a,b| [a.grant_id,a.id] <=> [b.grant_id,b.id] }
+
+      @grant_submissions = @grant_submissions.sort { |a,b| [a.name] <=> [b.name] }
+
+      @votes = Hash.new
+
+      @grant_submissions.each do |gs|
+        gs.class_eval do
+          attr_accessor :assigned
         end
 
-        @grant_submissions = GrantSubmission.where(grant_id: active_grants)
+        vote = Vote.where("voter_id = ? AND grant_submission_id = ?", current_voter.id, gs.id).take
 
-        # this is good for lace/temple
-        # @grant_submissions = @grant_submissions.sort { |a,b| [a.grant_id,a.id] <=> [b.grant_id,b.id] }
-
-        @grant_submissions = @grant_submissions.sort { |a,b| [a.name] <=> [b.name] }
-
-        @votes = Hash.new
-
-        @grant_submissions.each do |gs|
-            gs.class_eval do
-                attr_accessor :assigned
-            end
-
-            vote = Vote.where("voter_id = ? AND grant_submission_id = ?", current_voter.id, gs.id).take
-
-            if(!vote)
-                vote = Vote.new
-                vote.voter_id = current_voter.id
-                vote.grant_submission_id = gs.id
-                vote.save
-            end
-
-            @votes[gs.id] = vote
-
-            #assignments
-            vsa = VoterSubmissionAssignment.where("voter = ? AND grant_submission = ?", current_voter.id, gs.id).take
-            if(vsa)
-                gs.assigned = 1
-            else
-                gs.assigned = 0
-            end
-
+        if(!vote)
+          vote = Vote.new
+          vote.voter_id = current_voter.id
+          vote.grant_submission_id = gs.id
+          vote.save
         end
 
-        @grant_submissions_assigned = @grant_submissions.select{|gs| gs.assigned == 1}
-        @grant_submissions_unassigned = @grant_submissions.select{|gs| gs.assigned == 0}
+        @votes[gs.id] = vote
 
-        #@grant_submissions_unassigned = GrantSubmission.where(grant_id: [1,2])
-        @grant_submissions_unassigned.sort_by {|gs| gs.grant_id}
-        
+        #assignments
+        vsa = VoterSubmissionAssignment.where("voter = ? AND grant_submission = ?", current_voter.id, gs.id).take
+        if(vsa)
+          gs.assigned = 1
+        else
+          gs.assigned = 0
+        end
+      end
+
+      @grant_submissions_assigned = @grant_submissions.select{|gs| gs.assigned == 1}
+      @grant_submissions_unassigned = @grant_submissions.select{|gs| gs.assigned == 0}
+
+      #@grant_submissions_unassigned = GrantSubmission.where(grant_id: [1,2])
+      @grant_submissions_unassigned.sort_by {|gs| gs.grant_id}
     end
 
   def vote
@@ -96,11 +94,11 @@ class VotersController < ApplicationController
     # Go through all of the votes and submit all of the values even if they
     # haven't changed :(
     @grant_submissions.each do |gs|
-        vote = Vote.where("voter_id = ? AND grant_submission_id = ?", current_voter.id, gs.id).take
-        vote.score_t = params['t'][gs.id.to_s]
-        vote.score_c = params['c'][gs.id.to_s]
-        vote.score_f = params['f'][gs.id.to_s]
-        vote.save
+      vote = Vote.where("voter_id = ? AND grant_submission_id = ?", current_voter.id, gs.id).take
+      vote.score_t = params['t'][gs.id.to_s]
+      vote.score_c = params['c'][gs.id.to_s]
+      vote.score_f = params['f'][gs.id.to_s]
+      vote.save
     end
 
     redirect_to action: "index"
