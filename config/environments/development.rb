cb92eb32213e5c7a0a92b9941f3e2e2ecf2c35f9
Owen Williams <owilliams@mixxx.org>
2016-10-09 16:16:15 -0400

    small tweaks

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
  
+  # This is the hostname that will be used in links inside emails
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.action_mailer.delivery_method = :smtp
