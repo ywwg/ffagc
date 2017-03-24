0c9b564bf46c61e609d76fb36b6ee32e24dff9c3
katee <hello@kate.io>
2017-03-24 12:59:36 -0400

    Add specs for VotersController#create

--- spec/controllers/voters_controller_spec.rb
+++ spec/controllers/voters_controller_spec.rb
@@ -5,4 +5,39 @@ describe VotersController do
       expect(response).to be_ok
     end
   end
+
+  describe "#create" do
+    let(:voter_params) { FactoryGirl.attributes_for(:voter) }
+    let(:voter_survey_params) { FactoryGirl.attributes_for(:voter_survey) }
+    let(:grants_voter) { FactoryGirl.attributes_for(:grants_voter) }
+    let(:params) do
+      {
+        voter: voter_params,
+        survey: voter_survey_params,
+        grants_voters: [grants_voter],
+      }
+    end
+
+    def go!
+      post 'create', params
+    end
+
+    it 'returns ok' do
+      go!
+      expect(response).to be_ok
+    end
+
+    it 'creates a voter' do
+      expect { go! }.to change(Voter, :count).by(1)
+    end
+
+    context 'with existing voter' do
+      let!(:existing_voter) { FactoryGirl.create(:voter, :activated) }
+
+      it 'returns an error' do
+        post 'create', { voter: { email: existing_voter.email } }
+        expect(response).to render_template('signup_failure')
+      end
+    end
+  end
 end
