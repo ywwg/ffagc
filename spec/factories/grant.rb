c74d66929c73ff2abe35eacdcc76f53ddeec33c3
katee <hello@kate.io>
2017-03-25 00:15:48 -0400

    Update Grant factory to pass validation

--- spec/factories/grant.rb
+++ spec/factories/grant.rb
@@ -2,5 +2,12 @@ FactoryGirl.define do
   factory :grant do
     name { Faker::Hipster.sentence }
     hidden false
+    max_funding_dollars { 5_000 }
+    submit_start { Date.today }
+    submit_end { 1.week.from_now }
+    vote_start { 1.week.from_now }
+    vote_end { 4.weeks.from_now }
+    meeting_one { 2.weeks.from_now }
+    meeting_two { 3.weeks.from_now }
   end
 end
