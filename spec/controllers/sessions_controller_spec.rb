80726228016278471ff4ba45e99034ae72a99f39
katee <hello@kate.io>
2017-03-25 22:24:59 -0400

    Add SessionsController spec based on logic that was in sign_in_helper

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,61 @@
+describe SessionsController do
+  describe '#create_artist' do
+    let!(:artist) { FactoryGirl.create(:artist, :activated) }
+
+    it 'allows logging in with case-insensitive email' do
+      post 'create_artist', { session: { email: artist.email.upcase, password: artist.password } }
+      expect(response).to redirect_to('/artists')
+      #post artists_login_path, { email: user.email, password: password }
+      #assert_redirected_to '/artists'
+    end
+
+    context 'with inactive artist' do
+      let!(:inactive_artist) { FactoryGirl.create(:artist) }
+
+      it 'redirects to account activation' do
+        post 'create_artist', { session: { email: inactive_artist.email, password: inactive_artist.password } }
+        expect(response).to redirect_to(account_activations_unactivated_path(email: inactive_artist.email, type: 'artists'))
+      end
+    end
+  end
+
+  describe '#create_voter' do
+    let!(:voter) { FactoryGirl.create(:voter, :activated) }
+
+    it 'allows logging in with case-insensitive email' do
+      post 'create_voter', { session: { email: voter.email.upcase, password: voter.password } }
+      expect(response).to redirect_to('/voters')
+      #post voters_login_path, { email: user.email, password: password }
+      #assert_redirected_to '/voters'
+    end
+
+    context 'with inactive voter' do
+      let!(:inactive_voter) { FactoryGirl.create(:voter) }
+
+      it 'redirects to account activation' do
+        post 'create_voter', { session: { email: inactive_voter.email, password: inactive_voter.password } }
+        expect(response).to redirect_to(account_activations_unactivated_path(email: inactive_voter.email, type: 'voters'))
+      end
+    end
+  end
+
+  describe '#create_admin' do
+    let!(:admin) { FactoryGirl.create(:admin, :activated) }
+
+    it 'allows logging in with case-insensitive email' do
+      post 'create_admin', { session: { email: admin.email.upcase, password: admin.password } }
+      expect(response).to redirect_to('/admins')
+      #post admins_login_path, { email: user.email, password: password }
+      #assert_redirected_to '/admins'
+    end
+
+    context 'with inactive admin' do
+      let!(:inactive_admin) { FactoryGirl.create(:admin) }
+
+      it 'redirects to account activation' do
+        post 'create_admin', { session: { email: inactive_admin.email, password: inactive_admin.password } }
+        expect(response).to redirect_to(account_activations_unactivated_path(email: inactive_admin.email, type: 'admins'))
+      end
+    end
+  end
+end
