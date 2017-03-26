3cb10610b6e581189be48c2c2de557f95dbf65f5
katee <hello@kate.io>
2017-03-25 23:51:56 -0400

    Rspecize voter assignment test

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -44,49 +44,44 @@ describe AdminsController do
 
         # Make sure each submission has the expected number of assigned voters
         GrantSubmission.all.each do |gs|
-          grant = Grant.find(gs.grant_id)
-          expect_voters = 3
-          # Second grant only has two applicable voters, so each submission will
-          # only get two voters.
-          if grant.id == 2
-            expect_voters = 2
-          end
           vsas = VoterSubmissionAssignment.where(grant_submission_id: gs.id)
-          assert vsas.count == expect_voters
+
+          if gs.grant == grant_2
+            expect(vsas.count).to eq(2)
+          else
+            expect(vsas.count).to eq(3)
+          end
+
           verify_assignments(gs.id)
         end
 
-        # Make sure no dupe assignments
+        # Make sure there are no dupe assignments
         Voter.all.each do |v|
-          last = -1
-          VoterSubmissionAssignment.where(voter_id: v.id)
-              .order(grant_submission_id: :asc).each do |vsa|
-            assert vsa.grant_submission_id != last
-            last = vsa.grant_submission_id
-          end
+          vsas_ids = VoterSubmissionAssignment.where(voter_id: v.id).pluck(:id)
+          expect(vsas_ids).to eq(vsas_ids.uniq)
         end
 
         # Make sure voters have an even number of assignments
         # Urmam will have 4 or 5
         vsas = VoterSubmissionAssignment.where(voter_id: 1)
-        assert vsas.count == 4 || vsas.count == 5
+        expect(vsas.count).to be_between(4, 5)
 
         # ids 2 through 5 are inactive so they have none
         Voter.where("id >= 2 AND id <= 5").each do |v|
           vsas = VoterSubmissionAssignment.where(voter_id: v.id)
-          assert vsas.count == 0
+          expect(vsas.count).to eq(0)
         end
 
         # ids 6 and 7 will have a ton because they can vote in the second batch
         Voter.where("id >= 6 AND id <= 7").each do |v|
           vsas = VoterSubmissionAssignment.where(voter_id: v.id)
-          assert vsas.count == 14 || vsas.count == 15
+          expect(vsas.count).to be_between(14, 15)
         end
 
         # the rest will have 4 or 5
         Voter.where("id >= 8").each do |v|
           vsas = VoterSubmissionAssignment.where(voter_id: v.id)
-          assert vsas.count == 4 || vsas.count == 5
+          expect(vsas.count).to be_between(4, 5)
         end
       end
     end
