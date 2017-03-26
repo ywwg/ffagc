c17e2637424d60c25f915b90a047dc8d9a73bdee
katee <hello@kate.io>
2017-03-26 15:54:14 -0400

    Use different hash syntax

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -2,14 +2,14 @@ class Grant < ActiveRecord::Base
   has_many :grants_voters
   has_many :grant_submissions
 
-  validates :name, :presence => true
-  validates :max_funding_dollars, :presence => true, :numericality => {:greater_than => 0, :only_integer => true}
-  validates :submit_start, :presence => true
-  validates :submit_end, :presence => true
-  validates :vote_start, :presence => true
-  validates :vote_end, :presence => true
-  validates :meeting_one, :presence => true
-  validates :meeting_two, :presence => true
+  validates :name, presence: true
+  validates :max_funding_dollars, presence: true, numericality: { greater_than: 0, only_integer: true}
+  validates :submit_start,  presence: true
+  validates :submit_end,  presence: true
+  validates :vote_start,  presence: true
+  validates :vote_end,  presence: true
+  validates :meeting_one,  presence: true
+  validates :meeting_two,  presence: true
   # Currently the only place the "hidden" attribute is used is in the voter
   # signup page.  The intent is that some grants may be private and only voted
   # on by the Art Core.  Admins should modify individual voters to add them
