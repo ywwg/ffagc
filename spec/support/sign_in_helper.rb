79d6e81cda7b4035c0c5425e5281c81d8bf07eea
katee <hello@kate.io>
2017-03-31 11:57:00 -0400

    Add easier to use sign in helper

--- spec/support/sign_in_helper.rb
+++ spec/support/sign_in_helper.rb
@@ -1,3 +1,13 @@
+def sign_in(user)
+  if user.is_a?(Admin)
+    sign_in_admin(user.id)
+  elsif user.is_a?(Artist)
+    sign_in_artist(user.id)
+  elsif user.is_a?(Voter)
+    sign_in_voter(user.id)
+  end
+end
+
 def sign_in_artist(artist_id)
   @request.session['artist_id'] = artist_id
 end
