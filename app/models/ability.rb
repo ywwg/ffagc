ac5d7aab53e4d807c99d5cfb48b27276d87e2d6f
katee <hello@kate.io>
2017-03-31 11:21:36 -0400

    Add first pass of cancan abilities

---
+++ app/models/ability.rb
@@ -0,0 +1,32 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+
+    # TODO currently checks if it has the class, in the future it will check
+    # a `roles` attribute or similar
+
+    # TODO using `voter_id`, `artist_id` which will become `user_id` in future
+    # after models are unified
+
+    # TODO why do admins not require activation?
+    if user.is_a?(Admin)
+      can :manage, :all
+    end
+
+    if user.is_a?(Artist) && user.activated?
+      can :manage, GrantSubmission, artist_id: user.id
+      can :manage, ArtistSurvey, artist_id: user.id
+      can :manage, Proposal, grant_submission_id: user.grant_submission_ids
+    end
+
+    if user.is_a?(Voter) && user.activated?
+      can :manage, VoterSurvey, voter_id: user.id
+      can :manage, Vote, voter_id: user.id
+
+      can :read, GrantSubmission
+      can :read, VoterSubmissionAssignment, voter_id: user.id
+    end
+  end
+end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,84 @@
+describe Ability do
+  subject(:ability) { Ability.new(user) }
+
+  context 'with admin' do
+    let(:user) { FactoryGirl.create(:admin) }
+
+    it { is_expected.to be_able_to(:manage, Admin.new) }
+    it { is_expected.to be_able_to(:manage, ArtistSurvey.new) }
+    it { is_expected.to be_able_to(:manage, Artist.new) }
+    it { is_expected.to be_able_to(:manage, GrantSubmission.new) }
+    it { is_expected.to be_able_to(:manage, Grant.new) }
+    it { is_expected.to be_able_to(:manage, GrantsVoter.new) }
+    it { is_expected.to be_able_to(:manage, Proposal.new) }
+    it { is_expected.to be_able_to(:manage, VoterSubmissionAssignment.new) }
+    it { is_expected.to be_able_to(:manage, VoterSurvey.new) }
+    it { is_expected.to be_able_to(:manage, Voter.new) }
+    it { is_expected.to be_able_to(:manage, Vote.new) }
+  end
+
+  context 'with artist' do
+    let!(:user) { FactoryGirl.create(:artist, :activated) }
+    let!(:artist_survey) { FactoryGirl.create(:artist_survey, artist: user) }
+    let!(:grant_submission) { FactoryGirl.create(:grant_submission, artist: user) }
+    let!(:proposal) { FactoryGirl.create(:proposal, grant_submission: grant_submission) }
+
+    it { is_expected.to be_able_to(:manage, artist_survey) }
+    it { is_expected.to be_able_to(:manage, grant_submission) }
+    it { is_expected.to be_able_to(:manage, proposal) }
+
+    it { is_expected.not_to be_able_to(:manage, Admin.new) }
+    it { is_expected.not_to be_able_to(:manage, ArtistSurvey.new) }
+    it { is_expected.not_to be_able_to(:manage, Artist.new) }
+    it { is_expected.not_to be_able_to(:manage, GrantSubmission.new) }
+    it { is_expected.not_to be_able_to(:manage, Grant.new) }
+    it { is_expected.not_to be_able_to(:manage, GrantsVoter.new) }
+    it { is_expected.not_to be_able_to(:manage, Proposal.new) }
+    it { is_expected.not_to be_able_to(:manage, VoterSubmissionAssignment.new) }
+    it { is_expected.not_to be_able_to(:manage, VoterSurvey.new) }
+    it { is_expected.not_to be_able_to(:manage, Voter.new) }
+    it { is_expected.not_to be_able_to(:manage, Vote.new) }
+
+    context 'when not activated' do
+      let!(:user) { FactoryGirl.create(:artist) }
+
+      it { is_expected.not_to be_able_to(:manage, artist_survey) }
+      it { is_expected.not_to be_able_to(:manage, grant_submission) }
+      it { is_expected.not_to be_able_to(:manage, proposal) }
+    end
+  end
+
+  context 'with voter' do
+    let(:user) { FactoryGirl.create(:voter, :activated) }
+
+    let(:voter_submission_assignment) { FactoryGirl.build(:voter_submission_assignment, voter: user) }
+    let(:voter_survey) { FactoryGirl.create(:voter_survey, voter: user) }
+    let(:vote) { FactoryGirl.build(:vote, voter: user) }
+
+    it { is_expected.to be_able_to(:manage, vote) }
+    it { is_expected.to be_able_to(:manage, voter_survey) }
+    it { is_expected.to be_able_to(:read, voter_submission_assignment) }
+    it { is_expected.to be_able_to(:read, GrantSubmission.new) }
+
+    it { is_expected.not_to be_able_to(:manage, Admin.new) }
+    it { is_expected.not_to be_able_to(:manage, ArtistSurvey.new) }
+    it { is_expected.not_to be_able_to(:manage, Artist.new) }
+    it { is_expected.not_to be_able_to(:manage, GrantSubmission.new) }
+    it { is_expected.not_to be_able_to(:manage, Grant.new) }
+    it { is_expected.not_to be_able_to(:manage, GrantsVoter.new) }
+    it { is_expected.not_to be_able_to(:manage, Proposal.new) }
+    it { is_expected.not_to be_able_to(:manage, VoterSubmissionAssignment.new) }
+    it { is_expected.not_to be_able_to(:manage, VoterSurvey.new) }
+    it { is_expected.not_to be_able_to(:manage, Voter.new) }
+    it { is_expected.not_to be_able_to(:manage, Vote.new) }
+
+    context 'when not activated' do
+      let(:user) { FactoryGirl.create(:voter) }
+
+      it { is_expected.not_to be_able_to(:manage, vote) }
+      it { is_expected.not_to be_able_to(:manage, voter_survey) }
+      it { is_expected.not_to be_able_to(:read, voter_submission_assignment) }
+      it { is_expected.not_to be_able_to(:read, GrantSubmission.new) }
+    end
+  end
+end
