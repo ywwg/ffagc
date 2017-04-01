c22c22aa12b6a0116a701c8aefdb247880f30484
katee <hello@kate.io>
2017-04-01 19:59:04 -0400

    Correctly format question/answer blockquote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def safe_blockquote(html_text)
-    sanitize(html_text).gsub("\n", "<br />").html_safe
+    sanitize(html_text)&.gsub("\n", "<br />")&.html_safe
   end
 end
