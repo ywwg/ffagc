8b79cb1e10d91f8e694c05c84f83b3ae5b542aa7
katee <hello@kate.io>
2017-04-02 17:49:39 -0400

    Add spec for ProposalsController

---
+++ spec/controllers/proposals_controller_spec.rb
@@ -0,0 +1,54 @@
+describe ProposalsController do
+  let!(:artist) { FactoryGirl.create(:artist, :activated) }
+  let!(:grant_submission) { FactoryGirl.create(:grant_submission, artist: artist) }
+  let!(:proposal) { FactoryGirl.create(:proposal, grant_submission: grant_submission) }
+
+  describe '#destroy' do
+    def go!(id)
+      delete 'destroy', id: id
+    end
+
+    subject { response }
+
+    context 'artist signed in' do
+      before { sign_in artist }
+
+      context 'with valid submission for artist' do
+        it 'deletes submission' do
+          expect { go! proposal.id }.to change { Proposal.count }.by(-1)
+
+          expect(Proposal.where(id: proposal.id)).not_to exist
+
+          expect(response).to redirect_to(edit_grant_submission_path(grant_submission))
+        end
+      end
+
+      context 'with non-existent proposal id' do
+        it 'does not delete any proposals' do
+          expect { go! Proposal.count + 1 }.not_to change { Proposal.count }
+        end
+      end
+
+      context 'with proposal id for another artist' do
+        let!(:another_proposal) { FactoryGirl.create(:proposal) }
+
+        it 'does not delete any submissions' do
+          expect { go! another_proposal.id }.not_to change { Proposal.count }
+        end
+      end
+    end
+
+    context 'voter signed in' do
+      let!(:voter) { FactoryGirl.create(:voter, :activated) }
+
+      before { sign_in voter }
+
+      context 'with valid submission' do
+        it 'deletes submission' do
+          expect { go! proposal.id }.not_to change { Proposal.count }
+          expect(response).to be_forbidden
+        end
+      end
+    end
+  end
+end
