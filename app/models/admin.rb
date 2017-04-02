97361ad98db254fa5a874604e6e308e69bb3ec1f
katee <hello@kate.io>
2017-04-02 08:42:08 -0400

    Normalize email and activate admin before validation

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -10,4 +10,17 @@ class Admin < ActiveRecord::Base
   validates :password, length: { minimum: 4 }, on: :create
 
   validates_confirmation_of :password, on: :create
+
+  before_validation :normalize_email, :activate
+
+  private
+
+  def normalize_email
+    self.email = email.downcase.strip
+  end
+
+  def activate
+    self.activated = true
+    self.activated_at = Time.now
+  end
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,4 +2,18 @@ describe Admin do
   subject { FactoryGirl.build(:admin) }
 
   include_examples 'password reset model', 'admins'
+
+  context 'before_validation' do
+    subject { FactoryGirl.build(:admin, email: '  UPPERCASE@example.com ') }
+
+    it 'normalizes email' do
+      expect { subject.save! }.to change { subject.email }
+      expect(subject.reload.email).to eq('uppercase@example.com')
+    end
+
+    it 'activates' do
+      expect { subject.save! }.to change { subject.activated }
+      expect(subject.activated).to eq(true)
+    end
+  end
 end
