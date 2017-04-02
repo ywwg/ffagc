b18394ea5dfd409e8a588837e5d4756883d0b49c
katee <hello@kate.io>
2017-04-02 09:16:47 -0400

    Add #index, #new, and #create specs for AdminsController

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -1,4 +1,90 @@
 describe AdminsController do
+  subject { response }
+
+  describe '#index' do
+    def go!
+      get :index
+    end
+
+    context 'with valid params' do
+      let(:admin_params) { FactoryGirl.attributes_for(:admin) }
+
+      it 'creates Admin' do
+        expect { go! }.to change { Admin.count }.by(1)
+        expect(response).to redirect_to(root_path)
+      end
+
+      context 'when an Admin already exists' do
+        let!(:admin) { FactoryGirl.create(:admin) }
+
+        it { go!; is_expected.to be_forbidden}
+
+        context 'when admin signed in' do
+          before do
+            sign_in admin
+          end
+
+          it 'creates Admin' do
+            expect { go! }.to change { Admin.count }.by(1)
+            expect(response).to render_template('new')
+            expect(flash.keys).to include('success')
+          end
+        end
+      end
+    end
+  end
+
+  describe '#new' do
+    def go!
+      get :new
+    end
+
+    before { go! }
+
+    it { is_expected.to render_template('new') }
+  end
+
+  describe '#create' do
+    def go!
+      post :create, admin: admin_params
+    end
+
+    let(:admin_params) { FactoryGirl.attributes_for(:admin) }
+
+    it 'creates Admin' do
+      expect { go! }.to change { Admin.count }.by(1)
+      expect(response).to redirect_to(root_path)
+    end
+
+    context 'when an Admin already exists' do
+      let!(:admin) { FactoryGirl.create(:admin) }
+
+      it { go!; is_expected.to be_forbidden}
+
+      context 'when admin signed in' do
+        before do
+          sign_in admin
+        end
+
+        it 'creates Admin' do
+          expect { go! }.to change { Admin.count }.by(1)
+          expect(response).to render_template('new')
+          expect(flash.keys).to include('success')
+        end
+      end
+    end
+
+    context 'with invalid params' do
+      let!(:existing_admin) { FactoryGirl.create(:admin) }
+      let(:admin_params) { FactoryGirl.attributes_for(:admin, email: existing_admin.email) }
+
+      it 'displays form' do
+        expect { go! }.not_to change { Admin.count }
+        expect(response).to render_template('new')
+      end
+    end
+  end
+
   describe '#assign' do
     def go!
       post 'assign'
