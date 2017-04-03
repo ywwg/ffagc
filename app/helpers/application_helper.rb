b50e56a9645b45191dd26c709400ad0a8aa4185c
katee <hello@kate.io>
2017-04-02 21:20:07 -0400

    Improve display of dates for Grants

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def safe_blockquote(html_text)
     sanitize(html_text)&.gsub("\n", "<br />")&.html_safe
   end
+
+  def humanize_date(date)
+    suffix = if date.past?
+      'ago'
+    else
+      'from now'
+    end
+
+    "#{distance_of_time_in_words_to_now(date)} #{suffix}"
+  end
 end
