59c9983b9af173e85a9e54a2c2ac9f269a6baf58
katee <hello@kate.io>
2017-04-02 21:35:55 -0400

    Tabbing

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -1,5 +1,5 @@
 class VotersController < ApplicationController
-    before_filter :initialize_voter
+  before_filter :initialize_voter
 
   def show
     @voter = Voter.find(params[:id])
@@ -8,148 +8,148 @@ class VotersController < ApplicationController
     @grants = Grant.all
   end
 
-    def signup
+  def signup
+  end
+
+  def create
+    if Voter.exists?(email: voter_params[:email.downcase])
+      flash[:warning] = "The email address #{voter_params[:email.downcase]} already exists in our system"
+      render "signup_failure"
+      return
     end
 
-    def create
-      if Voter.exists?(email: voter_params[:email.downcase])
-        flash[:warning] = "The email address #{voter_params[:email.downcase]} already exists in our system"
-        render "signup_failure"
-        return
-      end
+    @voter = Voter.new(voter_params)
 
-      @voter = Voter.new(voter_params)
+    @voter.email = @voter.email.downcase
 
-      @voter.email = @voter.email.downcase
+    if @voter.save
+      # save survey
+      voter_survey = VoterSurvey.new(voter_survey_params)
+      voter_survey.voter_id = @voter.id
+      voter_survey.save
 
-      if @voter.save
-        # save survey
-        voter_survey = VoterSurvey.new(voter_survey_params)
-        voter_survey.voter_id = @voter.id
-        voter_survey.save
+      # save participation info
+      meetings = collated_meetings
 
-        # save participation info
-        meetings = collated_meetings
-
-        voter_participation_params.each do |collated_id, can_do|
-          if can_do == "0"
+      voter_participation_params.each do |collated_id, can_do|
+        if can_do == "0"
+          next
+        end
+        # We have to map from collated index to the list of grants.
+        # This is a gross n*m walk through the lists, but we are talking very
+        # small lists.
+        meetings.each do |dates, data|
+          if data['id'].to_s != collated_id
             next
           end
-          # We have to map from collated index to the list of grants.
-          # This is a gross n*m walk through the lists, but we are talking very
-          # small lists.
-          meetings.each do |dates, data|
-            if data['id'].to_s != collated_id
+          data['grant_ids'].each do |gid|
+            # sanity check that the grant id is real.
+            if Grant.find(gid) == nil
               next
             end
-            data['grant_ids'].each do |gid|
-              # sanity check that the grant id is real.
-              if Grant.find(gid) == nil
-                next
-              end
-              grants_voter = GrantsVoter.new
-              grants_voter.voter_id = @voter.id
-              grants_voter.grant_id = gid
-              grants_voter.save
-            end
+            grants_voter = GrantsVoter.new
+            grants_voter.voter_id = @voter.id
+            grants_voter.grant_id = gid
+            grants_voter.save
           end
         end
-
-        # Send email
-        begin
-          UserMailer.account_activation("voters", @voter).deliver_now
-          logger.info "email: voter account activation sent to #{@voter.email}"
-        rescue
-          flash[:warning] = "Error sending email confirmation"
-          render "signup_failure"
-          return
-        end
-
-        render "signup_success"
-      else
-        render "signup_failure"
       end
-    end
 
-    def update
-      # could allow for (timing based) Voter enumeration
-      @voter = Voter.find(params[:id])
-
-      unless can? :manage, GrantsVoter.new(voter: @voter)
-        redirect_to '/'
+      # Send email
+      begin
+        UserMailer.account_activation("voters", @voter).deliver_now
+        logger.info "email: voter account activation sent to #{@voter.email}"
+      rescue
+        flash[:warning] = "Error sending email confirmation"
+        render "signup_failure"
         return
       end
 
-      voter_participation_params.each do |grant_id, can_do|
-        unless Grant.find(grant_id).present?
+      render "signup_success"
+    else
+      render "signup_failure"
+    end
+  end
+
+  def update
+    # could allow for (timing based) Voter enumeration
+    @voter = Voter.find(params[:id])
+
+    unless can? :manage, GrantsVoter.new(voter: @voter)
+      redirect_to '/'
+      return
+    end
+
+    voter_participation_params.each do |grant_id, can_do|
+      unless Grant.find(grant_id).present?
+        next
+      end
+
+      if can_do == '0'
+        if GrantsVoter.exists?(voter_id: @voter.id, grant_id: grant_id)
+          GrantsVoter.find_by(voter_id: @voter.id, grant_id: grant_id).destroy
+        end
+      else
+        if GrantsVoter.exists?(voter_id: @voter.id, grant_id: grant_id)
           next
         end
 
-        if can_do == '0'
-          if GrantsVoter.exists?(voter_id: @voter.id, grant_id: grant_id)
-            GrantsVoter.find_by(voter_id: @voter.id, grant_id: grant_id).destroy
-          end
-        else
-          if GrantsVoter.exists?(voter_id: @voter.id, grant_id: grant_id)
-            next
-          end
-
-          grants_voter = GrantsVoter.create!(voter: @voter, grant_id: grant_id)
-        end
+        grants_voter = GrantsVoter.create!(voter: @voter, grant_id: grant_id)
       end
-
-      redirect_to controller: 'admins', action: 'voters'
     end
 
-    def index
-      unless can? :vote, GrantSubmission.new
-        flash[:warning] = 'You are not able to vote'
-        redirect_to '/'
-        return
+    redirect_to controller: 'admins', action: 'voters'
+  end
+
+  def index
+    unless can? :vote, GrantSubmission.new
+      flash[:warning] = 'You are not able to vote'
+      redirect_to '/'
+      return
+    end
+
+    # TODO: use scope
+    @grant_submissions = GrantSubmission.where(grant_id: voter_active_vote_grants(current_voter.id))
+
+    # TODO: sort in scope or add sorting scope
+    @grant_submissions = @grant_submissions.sort { |a,b| [a.name] <=> [b.name] }
+
+    @votes = Hash.new
+
+    @grant_submissions.each do |gs|
+      gs.class_eval do
+        attr_accessor :assigned
       end
 
-      # TODO: use scope
-      @grant_submissions = GrantSubmission.where(grant_id: voter_active_vote_grants(current_voter.id))
+      vote = current_voter.votes.where(grant_submission: gs).first_or_create
 
-      # TODO: sort in scope or add sorting scope
-      @grant_submissions = @grant_submissions.sort { |a,b| [a.name] <=> [b.name] }
+      @votes[gs.id] = vote
 
-      @votes = Hash.new
+      #assignments
+      vsa = current_voter.voter_submission_assignments.where(grant_submission: gs)
 
-      @grant_submissions.each do |gs|
-        gs.class_eval do
-          attr_accessor :assigned
-        end
-
-        vote = current_voter.votes.where(grant_submission: gs).first_or_create
-
-        @votes[gs.id] = vote
-
-        #assignments
-        vsa = current_voter.voter_submission_assignments.where(grant_submission: gs)
-
-        if vsa.exists?
-          gs.assigned = 1
-        else
-          gs.assigned = 0
-        end
-      end
-
-      # TODO: use scopes
-      @grant_submissions_assigned = @grant_submissions.select{|gs| gs.assigned == 1}
-      @grant_submissions_unassigned = @grant_submissions.select{|gs| gs.assigned == 0}
-
-      # TODO: sort in scope or add sorting scope
-      @grant_submissions_unassigned.sort_by {|gs| gs.grant_id}
-
-      if params[:all] == "true"
-        @show_all = true
-        @grant_submissions_display = @grant_submissions
+      if vsa.exists?
+        gs.assigned = 1
       else
-        @show_all = false
-        @grant_submissions_display = @grant_submissions_assigned
+        gs.assigned = 0
       end
     end
+
+    # TODO: use scopes
+    @grant_submissions_assigned = @grant_submissions.select{|gs| gs.assigned == 1}
+    @grant_submissions_unassigned = @grant_submissions.select{|gs| gs.assigned == 0}
+
+    # TODO: sort in scope or add sorting scope
+    @grant_submissions_unassigned.sort_by {|gs| gs.grant_id}
+
+    if params[:all] == "true"
+      @show_all = true
+      @grant_submissions_display = @grant_submissions
+    else
+      @show_all = false
+      @grant_submissions_display = @grant_submissions_assigned
+    end
+  end
 
   def vote
     @grant_submissions = GrantSubmission.where(grant_id: active_vote_grants)
