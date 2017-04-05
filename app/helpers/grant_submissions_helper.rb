985d5eef0de80bef9ff45d0f419b49d51e851ce2
katee <hello@kate.io>
2017-04-05 12:20:18 -0400

    Correctly display 'None' when there are neither questions or answers

--- app/helpers/grant_submissions_helper.rb
+++ app/helpers/grant_submissions_helper.rb
@@ -1,14 +1,13 @@
 module GrantSubmissionsHelper
   def discussion_status(grant_submission)
-    discuss = []
+    "#{q_and_a_status(grant_submission)} (#{pluralize(grant_submission.proposals.count, 'doc')})"
+  end
 
+  def q_and_a_status(grant_submission)
+    discuss = []
     discuss.push("Q") if grant_submission.has_questions?
     discuss.push("A") if grant_submission.has_answers?
-
-    status = discuss.join('&').squeeze(' ') || 'None'
-
-    status += ", #{pluralize(grant_submission.proposals.count, 'doc')}"
-
-    status
+    discuss.push("None") if discuss.empty?
+    discuss.join('&').squeeze(' ')
   end
 end
