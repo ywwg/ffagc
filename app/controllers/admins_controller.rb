c9d0d1c23353d40fc1a52472bb7a68cdc9b9e8be
Owen Williams <owilliams@mixxx.org>
2017-04-07 12:11:45 -0400

    even more monkey patching

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -88,7 +88,7 @@ class AdminsController < ApplicationController
         artist = Artist.where(id: gs.artist_id).take
         grant = Grant.where(id: gs.grant_id).take
         begin
-          if gs.granted_funding_dollars > 0
+          if gs.granted_funding_dollars != nil && gs.granted_funding_dollars > 0
             UserMailer.grant_funded(gs, artist, grant, event_year).deliver
             logger.info "email: grant funded sent to #{artist.email}"
           else
