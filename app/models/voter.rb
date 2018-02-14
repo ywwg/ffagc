69171f9eac8422332606eb9ed09d2b9900b85591
katee <hello@kate.io>
2018-02-14 16:31:06 -0500

    Use scope to create voter email list

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -19,13 +19,10 @@ class Voter < ActiveRecord::Base
 
   validates_confirmation_of :password, on: :create
 
-  scope :verified, -> { where(verified: true) }
+  scope :activated, -> { where(activated: true) }
+  scope :verified, -> { activated.where(verified: true) }
 
-  def self.verified_emails
-    email_list = []
-    Voter.where(activated: true, verified: true).each do |v|
-      email_list.push("#{v.name} <#{v.email}>")
-    end
-    return email_list
+  def as_email_recipient
+    "#{self.name} <#{self.email}>"
   end
 end
